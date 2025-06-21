1f3cd2ac8b1aeead3346c17e6347d9f72294bb72
Alexander Li <alxli@mit.edu>
2025-06-20 17:50:25 -0800

    deleted useless ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SinglePageLayout from "./components/SinglePageLayout";
@@ -10,19 +9,17 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SinglePageLayout />} />
-          <Route path="/projects" element={<SinglePageLayout />} />
-          <Route path="/research" element={<SinglePageLayout />} />
-          <Route path="/resume" element={<SinglePageLayout />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SinglePageLayout />} />
+        <Route path="/projects" element={<SinglePageLayout />} />
+        <Route path="/research" element={<SinglePageLayout />} />
+        <Route path="/resume" element={<SinglePageLayout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
